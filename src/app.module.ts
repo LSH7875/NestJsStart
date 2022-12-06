ac9dd05f8086336e087033133b19d9cd43224bbc
LSH7875 <tmdgml7875@gmail.com>
2022-12-06 18:19:38 +0900

    first

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { RoadmapModule } from './roadmap/roadmap.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [RoadmapModule],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
